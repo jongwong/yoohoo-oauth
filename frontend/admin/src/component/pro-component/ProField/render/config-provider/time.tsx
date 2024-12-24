4adc1ec6961deed3b6991f4dcaeeb93cc62483de
jongwong <jongwong@aliyun.com>
2024-12-24 12:11:01 +0800

    feat: valueType 时间

--- frontend/admin/src/component/pro-component/ProField/render/config-provider/time.tsx
+++ frontend/admin/src/component/pro-component/ProField/render/config-provider/time.tsx
@@ -3,7 +3,7 @@
  * */
 import React from 'react';
 import dayjs from 'dayjs';
-import { get, isNil, isNumber } from 'lodash';
+import { isNumber } from 'lodash';
 
 import { DatePicker, DatePickerProps } from 'antd';
 import {
@@ -13,7 +13,6 @@ import {
 import ProxyWrapped from '@/component/pro-component/ProxyWrapped';
 import { CommonProConfigType } from '@/component/pro-component/ProField/render';
 import { EMPTY_TEXT } from '@/component/pro-component/constant';
-import { getErrorMessageName } from '@/component/pro-component/utils/not-export';
 import { ElementOf } from '@/component/pro-component/types';
 
 const { RangePicker } = DatePicker;
@@ -24,7 +23,6 @@ export enum DefaultTimeValueTypeEnum {
 	Date = 'date',
 	// eslint-disable-next-line @typescript-eslint/no-shadow
 	RangePicker = 'range-picker',
-	RangePickerObject = 'range-picker-object',
 }
 
 declare const _valueType: [
@@ -134,84 +132,6 @@ const defaultTimeValueTypeMap: CommonProConfigType['valueTypeMap'] = {
 									  })
 									: undefined;
 								op?.onChange?.(newVal);
-							}}
-						/>
-					);
-				}}
-			</ProxyWrapped>
-		),
-	},
-
-	[DefaultTimeValueTypeEnum.RangePickerObject]: {
-		render: (t, r, _idx, { field }) => {
-			const [startDataIndex, endDataIndex] = field?.extraDataIndex || [];
-			if (isNil(startDataIndex) || isNil(endDataIndex)) {
-				const name = getErrorMessageName(field);
-				throw Error(
-					`Error in  ${name}.  missing attributes extraDataIndex, such as {extraDataIndex:[startDataIndex,endDataIndex]} `
-				);
-			}
-
-			if (get(r, startDataIndex) && get(r, endDataIndex)) {
-				// eslint-disable-next-line array-callback-return
-				const newVal = [get(r, startDataIndex), get(r, endDataIndex)].map(it => {
-					if (it) {
-						return dayjs(it)?.format('YYYY-MM-DD');
-					}
-					return it;
-				});
-				return newVal?.join(' ~ ');
-			}
-			return EMPTY_TEXT;
-		},
-		renderFormItem: (
-			t,
-			r: Record<string, any>,
-			{ column, form, formKey, index }: Record<string, any>
-		) => (
-			<ProxyWrapped>
-				{(op: any) => {
-					const [startDataIndex, endDataIndex] = column?.extraDataIndex || [];
-					if (isNil(startDataIndex) || isNil(endDataIndex)) {
-						const name = getErrorMessageName(column);
-						throw Error(
-							`Error in  ${name}.  missing attributes extraDataIndex, such as {extraDataIndex:[startDataIndex,endDataIndex]} `
-						);
-					}
-
-					const _r = r;
-
-					const val = [get(_r, startDataIndex), get(_r, endDataIndex)].map(it =>
-						it ? dayjs(it) : undefined
-					);
-
-					return (
-						<RangePicker
-							{...op}
-							value={val as any}
-							onChange={e => {
-								const val1 = e?.[0]?.startOf('date')?.valueOf();
-								const val2 = e?.[1]?.endOf('date')?.valueOf();
-								const ob = {
-									[startDataIndex]: val1,
-									[endDataIndex]: val2,
-								};
-								if (column?._useType === 'useFormatColumns') {
-									op?.onChange?.(ob);
-								} else {
-									form?.setFields?.([
-										{
-											name: isNumber(index) ? [index, startDataIndex] : startDataIndex,
-											value: val1,
-										},
-										{
-											name: isNumber(index) ? [index, endDataIndex] : endDataIndex,
-											value: val2,
-										},
-									]);
-									form?.validateFields?.([formKey]);
-									column?.fieldProps?.onChange?.(ob);
-								}
 							}}
 						/>
 					);
